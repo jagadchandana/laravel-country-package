cdad750dce8db06fafab623a6eed57ccf4a9f258
jagad <jagadchandana61@gmail.com>
2025-06-09 13:25:04 +0530

    first commit

---
+++ src/Country.php
@@ -0,0 +1,19 @@
+namespace JCS\Country;
+
+class Country
+{
+    public static function all()
+    {
+        return collect(json_decode(file_get_contents(__DIR__ . '/countries.json'), true));
+    }
+
+    public static function getByCode(string $code)
+    {
+        return self::all()->firstWhere('code', strtoupper($code));
+    }
+
+    public static function getByName(string $name)
+    {
+        return self::all()->firstWhere('name', $name);
+    }
+}
---
+++ src/CountryServiceProvider.php
@@ -0,0 +1,16 @@
+namespace JCS\Country;
+
+use Illuminate\Support\ServiceProvider;
+
+class CountryServiceProvider extends ServiceProvider
+{
+public function register()
+{
+// Binding optional
+}
+
+public function boot()
+{
+// Optional publishing logic
+}
+}
